a68c3be53431e5f0dee4389ee824e26bb4428b69
Lawren <imnatuu@gmail.com>
2018-08-21 19:40:15 -0300

    vidrotemp

---
+++ wp-content/themes/mi_tema/footer.php
@@ -0,0 +1,12 @@
+<? php wp_footer() ?>
+
+	<!-- Footer -->
+	<footer class="py-5 bg-dark">
+		<div class="container">
+			<p class="m-0 text-center text-white">Copyright &copy; Mi nuevo tema 2018</p>
+		</div>
+		<!-- /.container -->
+	</footer>
+
+	</body>
+</html>
---
+++ wp-content/themes/mi_tema/functions.php
@@ -0,0 +1,10 @@
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/portfolio-item.css" rel="stylesheet">
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
